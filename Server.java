d7bb49ca0957d0958dc89aa9702a04fcb61c76ed
blue18 <garcb969@icloud.com>
2018-11-08 20:24:15 -0800

    created socket object to receive incoming client request

--- Server.java
+++ Server.java
@@ -7,6 +7,7 @@ import java.net.*;
 
 public class Server {
 	public static void main(String[] args) throws IOException {
+		
 		// Server is listening
 		ServerSocket serverSocket = new ServerSocket(5000);
 
@@ -18,15 +19,23 @@ public class Server {
 			try {
 
 				// Socket object to receive incoming client request
+				socket = serverSocket.accept();
+				System.out.println("A new client is connected: " + socket);
 
 				// obtaining input and output client requests 
+				DataInputStream dataInputStream = new DataInputStream(socket.getInputStream());
+				DataOutputStream dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+				System.out.println("Assigning new thread for this client");
 
 				// create a new thread object 
+				Thread thread = new ClientHandler(socket, dataInputStream, dataOutputStream);
 
 				// invoking the start() method 
+				thread.start();
 
 			} catch (Exception e) {
-
+				socket.close();
 				e.printStackTrace();
 			}
 		}
