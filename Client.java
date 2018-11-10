02dd4bd48cf3756d5578e56dad3ed545c1be4550
blue18 <garcb969@icloud.com>
2018-11-09 17:05:26 -0800

    working on the client: the running loop

--- Client.java
+++ Client.java
@@ -4,6 +4,38 @@ import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        
+
+
+        try {
+
+            Scanner scanner = new Scanner(System.in);
+
+            // getting localhost ip 
+            InetAddress ip = InetAddress.getByName("localhost");
+
+            // establishing the connection with server port 5056
+            Socket socket = new Socket(ip, 5056);
+
+            // obtaining input and output stream 
+            DataInputStream dataInputStream = DataInputStream(socket.getInputStream());
+
+            DataOutputStream dataOutputStream = DataOutputStream(socket.getOutputStream());
+            
+            // a loop that creates an exchange of information between client and client handler
+            while (true) {
+
+                // printing date or time as requested by client 
+
+            }
+
+            // Closing resources
+            scanner.close();
+            dataInputStream.close();
+            dataOutputStream.close();
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
